5f46003548121cbac7e9f5797e151c52c473af6c
azamandivine290-cmyk <azamandivine290@gmail.com>
2026-07-17 20:28:10 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,2 +1,8 @@
 name  = "Tom" 
 print ("Hello, " + name)
+# Let's try adding numbers 
+item_sold = 5 
+price = 20
+total_revenue = item_sold * price 
+print ("Total revenue is:")
+print (total_revenue) 
